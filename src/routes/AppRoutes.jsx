f0c6c100a3857109c0b1eecc3cb138d7d4eac1e6
JodiKKY <111978028+JodiKKY@users.noreply.github.com>
2026-01-26 09:58:45 +0000

    new

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,20 +1,20 @@
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes } from "react-router-dom";
 import Homepage from "../Pages/Homepage";
-import  About from "../Pages/About";
-import  Contact from "../Pages/Contact";
-import Projects from '../Pages/Projects';
-
+import About from "../Pages/About";
+import Contact from "../Pages/Contact";
+import Projects from "../Pages/Projects";
 
 function AppRoutes() {
-    return (
-      <Routes>
-      <Route path="/" element={<Homepage/>} />
-      <Route path='/about' element={<About/>}/>
-        <Route path='/projects' element={<Projects/>}/>
-      <Route path='/contact' element={<Contact/>}/>
-    
-      
-</Routes>
+  return (
+    <Routes>
+      <Route path="/" element={<Homepage />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/projects" element={<Projects />} />
+      <Route path="/contact" element={<Contact />} />
+
+      {/* fallback */}
+      <Route path="*" element={<Homepage />} />
+    </Routes>
   );
 }
 
